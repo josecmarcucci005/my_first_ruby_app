4eb129482ca17bea9d27736ab212cbf7b1154fbd
ja011 <jose.carlos.gonzalez.marcucci@detusche-boerse.com>
2019-01-07 17:18:42 +0100

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'courses/index'
+
+  get 'greeter/helloworld' => "greeter#hello"
+  get 'greeter/goodbye'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
